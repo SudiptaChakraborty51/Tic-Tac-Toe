4d0745ea944580f655fe8122d2b5aae2ea288ab2
Sudipta <hisudipta26@gmail.com>
2022-09-24 22:52:50 +0530

    feat: showing icon or sign on user click

--- script.js
+++ script.js
@@ -2,10 +2,36 @@ const selectBox = document.querySelector(".select-box");
 const selectXBtn = selectBox.querySelector(".playerX");
 const selectOBtn = selectBox.querySelector(".playerO");
 const playBoard = document.querySelector(".play-board");
+const allBox = document.querySelectorAll("section span");
+const players = document.querySelector(".players");
 
 window.onload = ()=> {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
+        
+    }
     selectXBtn.onclick = ()=> {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
     }
+    selectOBtn.onclick = ()=> {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+        players.setAttribute("class", "players active player");
+    }
+}
+
+let playerXIcon = "fas fa-times";
+let playerOIcon = "fa-regular fa-circle";
+
+//user click function
+function clickedBox(element) {
+    if(players.classList.contains("player")){
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.add("active");
+    }else {
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        players.classList.add("active");
+    }
+    element.style.pointerEvents = "none";
 }
