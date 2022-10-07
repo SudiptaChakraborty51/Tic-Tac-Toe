ad28501f90c6f1d9d473c0ccca8df7b08c365d81
Sudipta <hisudipta26@gmail.com>
2022-10-08 01:27:24 +0530

    feat: playersign set

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const players = document.querySelector(".players");
 
 window.onload = ()=> { //once window loaded
     for (let i = 0; i < allBox.length; i++) { //add onclick attribute in all available section's spans
-        allBox[i].setAttribute("onclick", "clickedBox(this)");
-        
+        allBox[i].setAttribute("onclick", "clickedBox(this)");   
     }
     selectXBtn.onclick = ()=> {
         selectBox.classList.add("hide"); //hide the select box on playerX button clicked
@@ -23,15 +22,20 @@ window.onload = ()=> { //once window loaded
 
 let playerXIcon = "fas fa-times"; 
 let playerOIcon = "fa-regular fa-circle";
+let playerSign = "X";
 
 //user click function
 function clickedBox(element) {
     if(players.classList.contains("player")){ //if players element has contains .player
         element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
         players.classList.add("active");
+        //if player will be O then we'll change the sign
+        playerSign = "O";
+        element.setAttribute("id", playerSign); 
     }else {
         element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element
         players.classList.add("active");
+        element.setAttribute("id", playerSign);
     }
     element.style.pointerEvents = "none"; //once user select any box then that box can't be selected again
     let randomDelayTime = ((Math.random()*1000)+200).toFixed(); //generating random time delay so bot will delay randomly to select box
@@ -42,6 +46,8 @@ function clickedBox(element) {
 
 //bot click function 
 function bot() {
+    //first change the playerSign...so if user has X value in id then bot will have O
+    playerSign = "O";
     let array=[]; //creating empty array...we'll store unselected box index in this array
     for(let i=0; i<allBox.length; i++) {
         if(allBox[i].childElementCount == 0) {//if span has no any child element
@@ -54,9 +60,13 @@ function bot() {
         if(players.classList.contains("player")){ //if players element has contains .player
             allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked allBox[randomBox]
             players.classList.remove("active");
+            //if user is O then the box id value will be X
+            playerSign = "X";
+            allBox[randomBox].setAttribute("id", playerSign);
         }else {
             allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
             players.classList.remove("active");
+            allBox[randomBox].setAttribute("id", playerSign);
         }
     }
     allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't select or click on that box again
