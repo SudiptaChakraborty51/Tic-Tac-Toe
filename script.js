a4cb22dd5368353950d869b53f9471c4a7cd9aa3
Sudipta <hisudipta26@gmail.com>
2022-10-08 21:49:38 +0530

    style: beautify three files

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function clickedBox(element) {
 
 //bot click function 
 function bot() {
-    if(runBot) {
+    if (runBot) {
         //first change the playerSign...so if user has X value in id then bot will have O
         playerSign = "O";
         let array = []; //creating empty array...we'll store unselected box index in this array
@@ -79,7 +79,7 @@ function bot() {
         }
         allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't select or click on that box again
         console.log(array);
-        playBoard.style.pointerEvents = "auto"; 
+        playBoard.style.pointerEvents = "auto";
         playerSign = "X"; //passing the x value
     }
 }
@@ -98,11 +98,11 @@ function checkClass(val1, val2, val3, sign) {
 
 function selectWinner() { //if one combination of them matched then select the winner
     if (checkClass(1, 2, 3, playerSign) || checkClass(4, 5, 6, playerSign) || checkClass(7, 8, 9, playerSign) || checkClass(1, 4, 7, playerSign) || checkClass(2, 5, 8, playerSign) || checkClass(3, 6, 9, playerSign) || checkClass(1, 5, 9, playerSign) || checkClass(3, 5, 7, playerSign)) {
-        console.log(playerSign+" "+" is the winner");
+        console.log(playerSign + " " + " is the winner");
         //once match won by someone then stop the bot
-        runBot=false;
+        runBot = false;
         bot(runBot);
-        setTimeout(()=>{ //we'll delay to show result box
+        setTimeout(() => { //we'll delay to show result box
             playBoard.classList.remove("show");
             resultBox.classList.add("show");
         }, 700);
@@ -110,10 +110,10 @@ function selectWinner() { //if one combination of them matched then select the w
     } else {
         //if match has drawn
         //first we'll check all id...if all span has id and no one won the game then we'll draw the game
-        if(getClass(1) != "" && getClass(2) != "" && getClass(3) != "" && getClass(4) != "" && getClass(5) != "" && getClass(6) != "" && getClass(7) != "" && getClass(8) != "" && getClass(9) != ""){
-            runBot=false;
+        if (getClass(1) != "" && getClass(2) != "" && getClass(3) != "" && getClass(4) != "" && getClass(5) != "" && getClass(6) != "" && getClass(7) != "" && getClass(8) != "" && getClass(9) != "") {
+            runBot = false;
             bot(runBot);
-            setTimeout(()=>{ //we'll delay to show result box
+            setTimeout(() => { //we'll delay to show result box
                 playBoard.classList.remove("show");
                 resultBox.classList.add("show");
             }, 700);
@@ -122,6 +122,6 @@ function selectWinner() { //if one combination of them matched then select the w
     }
 }
 
-replayBtn.onclick = ()=>{
+replayBtn.onclick = () => {
     window.location.reload(); //reload the current page
 }
